2bf000656fe3a4b879a8375f892fdca0a84913f6
bbarker63 <betterlivinghms@gmail.com>
2018-09-21 19:41:56 -0400

    forgot highest number comes first

--- print.rb
+++ print.rb
@@ -1,11 +1,11 @@
 101.times do |x|
 if x == 0
+elsif x % 15 == 0
+	print "throw"
 elsif x % 3 == 0
 	print "word"
 elsif x % 5 == 0
 	print "gang"
-elsif x % 15 == 0
-	print "throw"
 else
 	print x
 end
