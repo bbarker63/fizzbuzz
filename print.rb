07677a5f9b54068355d7ef6866604cfad6e6837b
bbarker63 <betterlivinghms@gmail.com>
2018-09-21 19:50:46 -0400

    tweaked from java example

--- print.rb
+++ print.rb
@@ -1,12 +1,3 @@
-101.times do |x|
-if x == 0
-elsif x % 15 == 0
-	print "throw"
-elsif x % 3 == 0
-	print "word"
-elsif x % 5 == 0
-	print "gang"
-else
-	print x
-end
+for num in (1..100)
+	print num
 end
