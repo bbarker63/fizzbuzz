39e7a19e1394dc54ec0e942c9d2636eac9b87cd7
bbarker63 <betterlivinghms@gmail.com>
2018-09-24 16:56:47 -0400

    added def and new function to calc

--- calc.rb
+++ calc.rb
@@ -1,6 +1,7 @@
 puts "simple calculator"
 print "<"
 
+
 numb = $stdin.gets.chomp.split(" ")
 
 num1 = numb[0]
@@ -9,7 +10,9 @@ num2 = numb[2]
 
 exit if num1 == "exit"
 
-puts case 
+
+def calculate(num1, opp, num2)
+puts case
 when opp == "+"
 	puts num1.to_f + num2.to_f
 when opp == "-"
@@ -20,7 +23,11 @@ when opp == "/"
 	puts num1.to_f / num2.to_f
 	else puts "rerun calculator"
 		load 'calc.rb'
+	end
 end
-
+puts calculate(num1, opp, num2)
 load 'calc.rb'
+
+
+
 
